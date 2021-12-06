c6bdd7f2deb5a8d43d34b192f2e2d1f72e24c2c8
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-12-06 15:12:25 -0800

    Got BlogCard to display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
 import { getBlogs } from './services/blogs';
+import Header from './components/layout/Header/Header';
+import Footer from './components/layout/Footer/Footer';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
@@ -8,14 +12,20 @@ function App() {
     const fetchData = async () => {
       const data = await getBlogs();
       setBlogs(data);
-      console.log(data);
     };
     fetchData();
   }, []);
   return (
-    <div className="App">
-      <h1>Blog List</h1>
-    </div>
+    <>
+      <Header />
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
+      <Footer />
+      <div className="App">
+        <h1>Blog List</h1>
+      </div>
+    </>
   );
 }
 
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,6 +2,5 @@ import { checkError, client } from './client';
 
 export async function getBlogs() {
   const response = await client.from('blogs').select();
-  console.log(response);
   return checkError(response);
 }
